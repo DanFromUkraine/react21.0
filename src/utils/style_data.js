49fe691cbaf024451a9feaa5dafe5a9a2e19026e
Kava <126253915+DanFromUkraine@users.noreply.github.com>
2024-07-21 18:02:01 +0300

    Add files via upload

---
+++ src/utils/style_data.js
@@ -0,0 +1,11 @@
+
+export const style_data = {
+    dark: {
+        background: "#333231",
+        textColor: "white"
+    },
+    light: {
+        background: "white",
+        textColor: "#333231"
+    }
+}
